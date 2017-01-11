d1c02b390029f3427df77610ef45a9998c8b31a6
roemhildtg <roemhildtg@gmail.com>
2017-01-11 15:31:05 -0600

    BUILD: make sure each module is listed in index for build

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 import 'spectre.css/dist/spectre.css';
 import 'font-awesome/css/font-awesome.css';
+import 'spectre-canjs/data-admin/';
+import 'spectre-canjs/filter-widget/';
+import 'spectre-canjs/form-widget/';
+import 'spectre-canjs/list-table/';
+import 'spectre-canjs/modal-dialog/';
+import 'spectre-canjs/nav-container/';
+import 'spectre-canjs/nav-list/';
+import 'spectre-canjs/paginate-widget/';
+import 'spectre-canjs/property-table/';
+import 'spectre-canjs/toast-container/';

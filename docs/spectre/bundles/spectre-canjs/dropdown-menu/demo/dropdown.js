720560918ab188b54c3730e68d2c4e1d54c3b177
Gregg Roemhildt <groemhildt@cityhall.com>
2017-01-20 09:25:22 -0600

    docs

--- docs/spectre/bundles/spectre-canjs/dropdown-menu/demo/dropdown.js
+++ docs/spectre/bundles/spectre-canjs/dropdown-menu/demo/dropdown.js
@@ -1,3 +1,3 @@
-/*spectre-canjs@0.21.1#dropdown-menu/demo/dropdown*/
-define("spectre-canjs@0.21.1#dropdown-menu/demo/dropdown",["can-stache","can-define/map/map","spectre-canjs/dropdown-menu/dropdown-menu"],function(e,n){"use strict";function d(e){return e&&e.__esModule?e:{"default":e}}var a=d(e),t=d(n),o=a["default"].from("demo-html"),r=new t["default"]({pages:[{label:"List"},{label:"Create",active:!0},{label:"Other Options"}]});document.body.appendChild(o(r))});
+/*spectre-canjs@0.21.3#dropdown-menu/demo/dropdown*/
+define("spectre-canjs@0.21.3#dropdown-menu/demo/dropdown",["can-stache","can-define/map/map","spectre-canjs/dropdown-menu/dropdown-menu"],function(e,n){"use strict";function d(e){return e&&e.__esModule?e:{"default":e}}var a=d(e),t=d(n),o=a["default"].from("demo-html"),r=new t["default"]({pages:[{label:"List"},{label:"Create",active:!0},{label:"Other Options"}]});document.body.appendChild(o(r))});
 //# sourceMappingURL=dropdown.js.map

44408ac67c686f8f5bee486f33885841a9084bb2
roemhildtg <roemhildtg@gmail.com>
2017-07-20 11:30:29 -0500

    update packages - roll back can-define

--- form-widget/demo/form.js
+++ form-widget/demo/form.js
@@ -9,13 +9,13 @@ import fixture from 'can-fixture';
 import stache from 'can-stache';
 import DefineMap from 'can-define/map/map';
 
-//this example uses fixtures to catch requests and simulate
-//file upload experience
+// this example uses fixtures to catch requests and simulate
+// file upload experience
 fixture.delay = 2000;
 fixture({
     method: 'POST',
     url: '/upload'
-}, function(request, response, headers, ajaxSettings) {
+}, function (request, response, headers, ajaxSettings) {
     return {
         uploads: ['fake_upload_filename']
     };
@@ -24,7 +24,7 @@ fixture({
 fixture({
     method: 'DELETE',
     url: '/upload'
-}, function() {
+}, function () {
     return {
         result: 'Success'
     };
@@ -34,8 +34,8 @@ fixture({
 const ChildObject = DefineMap.extend({
     json_field_1: 'string',
     json_field_2: {
-        validate(props){
-          return props.value != 2 ? 'The value must be 2' : undefined;
+        validate (props) {
+            return props.value != 2 ? 'The value must be 2' : undefined;
         },
         value: 2,
         fieldType: 'select',
@@ -49,97 +49,104 @@ const ChildObject = DefineMap.extend({
     },
     json_field_3: {
         type: 'number',
-        validate(props) {
+        validate (props) {
             return props.value < 10 ? 'Please enter a value greater than 10' : undefined;
         }
     },
     json_field_4: {
-      type: 'boolean',
-      fieldType: 'checkbox',
-      style: 'switch'
+        type: 'boolean',
+        fieldType: 'checkbox',
+        style: 'switch'
     }
-})
+});
 
 
 const Template = DefineMap.extend({
-    field1: { type: 'string', value: 'test-value' },
+    field1: {type: 'string', value: 'test-value'},
     field2: 'string',
     field3: 'number',
-    field4: 'date',
+    field4: {
+        type: 'date', 
+        value: new Date('2010-10-11')
+    },
     field5: 'string',
     field6: ChildObject
 });
 
 const fields = [{
-        name: 'field1',
-        validate(props) {
-            return props.value.length < 50 ? 'This field must contain at least 50 characters' : false;
-        }
-    },
-    {
-        value: 'another value',
-        name: 'field2',
-        alias: 'A basic textarea field',
-        type: 'text',
-        textarea: true,
-        placeholder: 'Enter the text: hello',
-        validate(props) {
-            if (props.value !== 'hello') {
-                return 'This field must contain the text, "hello"';
-            }
-            return false;
-        }
-    }, {
-        value: 1,
-        name: 'field3',
-        alias: 'A select dropdown',
-        fieldType: 'select',
-        options: [{
-            value: 1,
-            label: 'Option 1'
-        }, {
-            value: 2,
-            label: 'Option 2'
-        }]
-    }, {
-        name: 'field4',
-        alias: 'A date field',
-        fieldType: 'date',
-        value: new Date('2010-10-11')
-    }, {
-        name: 'field5',
-        alias: 'A file field',
-        multiple: true,
-        fieldType: 'file',
-        url: '/upload'
-    }, {
-        name: 'field6',
-        alias: 'A Subform Field',
-        fieldType: 'subform',
-        type: 'string',
-        value: '',
-        ObjectTemplate: ChildObject
+    name: 'field1',
+    validate (props) {
+        return props.value.length < 50 ? 'This field must contain at least 50 characters' : false;
     }
-]
+}, {
+    value: 'another value',
+    name: 'field2',
+    alias: 'A basic textarea field',
+    type: 'text',
+    textarea: true,
+    placeholder: 'Enter the text: hello',
+    validate (props) {
+        if (props.value !== 'hello') {
+            return 'This field must contain the text, "hello"';
+        }
+        return false;
+    }
+}, {
+    value: 1,
+    name: 'field3',
+    alias: 'A select dropdown',
+    fieldType: 'select',
+    options: [{
+        value: 1,
+        label: 'Option 1'
+    }, {
+        value: 2,
+        label: 'Option 2'
+    }]
+}, {
+    name: 'field4',
+    alias: 'A date field',
+    fieldType: 'date'
+}, {
+    name: 'field5',
+    alias: 'A file field',
+    multiple: true,
+    fieldType: 'file',
+    url: '/upload'
+}, {
+    name: 'field6',
+    alias: 'A Subform Field',
+    fieldType: 'subform',
+    type: 'string',
+    value: '',
+    ObjectTemplate: ChildObject
+}, {
+    name: 'field7',
+    alias: 'A checkbox',
+    fieldType: 'checkbox',
+    value: false
+    
+}];
 
 const render = stache.from('demo-html');
 
 const vm = new DefineMap({
-    dirtyObject: null,
     formObject: new Template(),
     fields: fields,
-    onChange() {
+    onChange () {
         console.log(arguments);
     },
-    onSubmit() {
+    onSubmit () {
         alert('Form submitted! See the console for details');
-        console.log('submitted data: ', this.formObject.serialize())
+        console.log('submitted data: ', this.formObject.serialize());
     },
-    onCancel() {
+    onCancel () {
         console.log('Form canceled!');
     },
-    stringify(obj) {
-      if(!obj){ return }
+    stringify (obj) {
+        if (!obj) {
+            return; 
+        }
         return JSON.stringify(obj.serialize());
     }
 });

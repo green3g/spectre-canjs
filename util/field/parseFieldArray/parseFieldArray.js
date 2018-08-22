ac28c87d8ea369e5ff0db7c21742fb5f1bda1705
Gregg Roemhildt <groemhildt@wsbeng.com>
2018-08-22 12:36:48 -0500

    add optional field type to parseFieldArray

--- util/field/parseFieldArray/parseFieldArray.js
+++ util/field/parseFieldArray/parseFieldArray.js
@@ -3,9 +3,10 @@ import Field from '../Field';
 /**
  * Converts an array of strings or field json objects into Field objects
  * @param  {Array<Field | String>} fields An array of either strings or JSON like objects representing Field object properties
+ * @param  {Constructor} Type Type of field to create and return
  * @return {Array<Field>} The array of fields
  */
-export default function parseFieldArray (fields) {
+export default function parseFieldArray (fields, Type = Field) {
     // create field objects
     return fields.map((f) => {
         // if we have a string give it a default name
@@ -15,6 +16,6 @@ export default function parseFieldArray (fields) {
             };
         }
         // add additional props with field constructor
-        return new Field(f);
+        return new Type(f);
     });
 }

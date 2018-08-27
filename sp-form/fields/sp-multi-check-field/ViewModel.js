1a9b826e2063becb75a9ed486e0cecc3167a9d26
Gregg Roemhildt <groemhildt@wsbeng.com>
2018-08-27 09:13:16 -0500

    allow list type to be converted to string type in multi-field

--- sp-form/fields/sp-multi-check-field/ViewModel.js
+++ sp-form/fields/sp-multi-check-field/ViewModel.js
@@ -12,13 +12,55 @@ export default SelectField.extend('CheckboxMulti', {
     /** @lends sp-multi-check-field.ViewModel.prototype */
     
     /**
-     * @type {Array<Any>}
+     * @type {Any}
      * @memberof sp-multi-check-field.ViewModel.prototype
-     * @description The current array values selected
+     * @description The current value of this field. Can be string or a list. 
      */
     value: {
+        set(val){
+            if(typeof val === 'string'){
+                this.valueType === 'string';
+                this.selectedValues = val.split(',');
+            }
+
+            return val;
+        }
+        // Type: DefineList,
+        // Default: DefineList
+    },
+    /**
+     * @type {Array<Any>}
+     * @memberof sp-multi-check-field.ViewModel.prototype
+     * @description The current array values selected. When this changes, it updates the value
+     */
+    selectedValues: {
         Type: DefineList,
-        Default: DefineList
+        Default: DefineList,
+        set(list){
+            list.on('length', () => {
+                let value = list;
+                if(this.valueType === 'string'){
+                    value = value.join(this.valueSeparator);
+                }
+                this.value = value;
+            });
+            return list;
+        }
+    },
+    /**
+     * @type {Array<Any>}
+     * @memberof sp-multi-check-field.ViewModel.prototype
+     * @description Type of value. Set this to string to coerce the list to a comma separated list. 
+     */
+    valueType: 'string',
+    /**
+     * @type {String}
+     * @memberof sp-multi-check-field.ViewModel.prototype
+     * @description Separator for the value string. List will be joined using this separator. Default is `,`. 
+     */
+    valueSeparator: {
+        type: 'string',
+        default: ','
     },
     /**
      * Dispatches the `fieldchange` event

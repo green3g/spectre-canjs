576275ac2634e9cafd290d9dbae0558646d830f2
Gregg Roemhildt <roemhildtg@gmail.com>
2018-09-04 21:11:06 -0500

    enhancements to file-list allowing for upload status

---
+++ sp-file-list-drop/sp-file-list-drop.js
@@ -0,0 +1,11 @@
+import Component from 'can-component';
+import DefineMap from 'can-define/map/map';
+
+// a droppable file list
+const ViewModel = DefineMap.extend('SpFileListDrop', {
+
+});
+export default Component.extend({
+    ViewModel,
+    view
+});
--- sp-file-list/ViewModel.js
+++ sp-file-list/ViewModel.js
@@ -4,6 +4,17 @@ import DefineList from 'can-define/list/list';
 
 export const img = new RegExp(/.*\.(?:jpg|jpeg|gif|png)/, 'i');
 
+export const FileMap = DefineMap.extend('FileMap', {
+    id: 'string',
+    uri: 'string',
+    isObjectURL: 'boolean',
+    progress: 'number'
+});
+
+export const FileList = DefineList.extend('FileList', {
+    '#': FileMap
+});
+
 /**
  * A `<sp-file-list />` component's ViewModel
  * 
@@ -17,7 +28,15 @@ export default DefineMap.extend('SPFileList', {
      * @memberof sp-file-list.ViewModel.prototype
      * @type {DefineList}
      */
-    files: DefineList,
+    files: FileList,
+    isDragOver: 'boolean',
+    isDropped: 'boolean',
+    isMouseOver: 'boolean',
+    maxProgressValue: {
+        type: 'number',
+        default: 100
+    },
+    el: '*',
     /**
      * Tests a file objects `id` property for image type extension
      * @memberof sp-file-list.ViewModel.prototype
@@ -26,5 +45,46 @@ export default DefineMap.extend('SPFileList', {
      */
     isImage (file) {
         return img.test(file.id);
+    },
+    remove (file) {
+        if (file.isObjectURL) {
+            window.URL.revokeObjectURL(file.uri);
+        }
+        const index = this.files.indexOf(file);
+        this.files.splice(index, 1);
+    },
+    addFiles (files, ev) {
+        if (ev) {
+            ev.preventDefault();
+        }
+        this.isDragOver = false;
+        files = Array.from(files).map((file) => {
+            const url = window.URL.createObjectURL(file);
+            return {
+                id: file.name,
+                uri: url,
+                isObjectURL: true,
+                progress: 1
+            };
+        });
+        this.files.replace(this.files.concat(files));
+    },
+    dragover (ev, isDragOver) {
+        ev.preventDefault();
+        this.isDragOver = isDragOver;
+    },
+    mouseover (ev, isMouseOver) {
+        ev.preventDefault();
+        this.isMouseOver = isMouseOver;
+    },
+    uploadClick (ev) {
+        ev.preventDefault();
+        this.el.click();
+    },
+    showProgress (progressValue) {
+        return typeof progressValue !== 'undefined' && progressValue < this.maxProgressValue;
+    },
+    connectedCallback (el) {
+        this.el = el.querySelector('input');
     }
 });

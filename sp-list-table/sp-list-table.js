102f5c336a0512eff54e9ce67c6bef1307872188
Gregg Roemhildt <roemhildtg@gmail.com>
2018-09-12 22:33:00 -0500

    add list table example

--- sp-list-table/sp-list-table.js
+++ sp-list-table/sp-list-table.js
@@ -11,7 +11,13 @@ import 'can-dom-data';
  * <iframe src="../sp-list-table/demo/index.html" style="border: 1px solid #ccc; width:100%; height:300px;"></iframe>
  * @module sp-list-table
  * @example 
- * <sp-list-table /> 
+ * <sp-list-table fields:from="fields" objects:from="objects" /> 
+ * {
+ *  fields: ['field_name', {
+ *      name: 'other_name',
+ *      displayComponent: '<strong>{{object[field.name]}}</strong>
+ *  }]
+ * }
  */
 export default Component.extend({
     tag: 'sp-list-table',
